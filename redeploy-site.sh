a75c0812ba62420fc421d3733534361343a430c4
aiejvn <139830381+aiejvn@users.noreply.github.com>
2024-07-12 16:13:28 -0400

    Switch from tmux to service hosting

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux list-sessions
-tmux kill-server
 cd /root/Portfolio-Site-MLH
 git fetch
 git reset origin/main --hard
@@ -9,7 +7,5 @@ chmod +x ./redeploy-site.sh
 cd ./python3-virtualenv
 source ./bin/activate
 pip install -r ../requirements.txt
-tmux new-session -d -s Portfolio-Website
-tmux send-keys -t 0 'cd /root/Portfolio-Site-MLH' C-m
-tmux send-keys -t 0 'export FLASK_ENV=development' C-m
-tmux send-keys -t 0 'flask run --host=0.0.0.0' C-m
+systemctl daemon-reload
+systemctl restart myportfolio
